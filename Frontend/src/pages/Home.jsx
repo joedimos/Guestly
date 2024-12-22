7573ac64402fd4e0d88a8c522b13cfe262721d59
Joseph Dimos <joedimos@gmail.com>
2024-12-22 10:08:00 -0500

    Create Home.jsx

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="p-8">
+      <h2 className="text-xl mb-4">Welcome to Guestly</h2>
+      <p>Your restaurant-customer review platform.</p>
+    </div>
+  );
+}
+
+export default Home;
